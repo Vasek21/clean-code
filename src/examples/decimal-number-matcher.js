e92f1da97575d32a8b65e931102d1360ae606291
Václav Šíma <vaclav.sima@plus4u.net>
2022-10-13 10:10:11 +0200

    Readability update & Error moved to map

--- src/examples/decimal-number-matcher.js
+++ src/examples/decimal-number-matcher.js
@@ -22,43 +22,53 @@ const MAX_NUM_OF_DIGITS_INDEX = 0;
 const MAX_NUM_OF_DECIMALS_INDEX = 1;
 const DECIMAL_VALIDATION_THRESHOLD = 1;
 
+const ERROR_MAP = {
+  conversionError: { code: "doubleNumber.e001", message: "The value is not a valid decimal number." },
+  digitError: { code: "doubleNumber.e002", message: "The value exceeded maximum number of digits." },
+  decimalError: { code: "doubleNumber.e003", message: "The value exceeded maximum number of decimal places." }
+};
+Object.freeze(ERROR_MAP);
+
 class DecimalNumberMatcher {
   constructor(...params) {
     this.params = params;
+    this.result = new ValidationResult();
   }
 
-  validateDigits(number, result) {
-    if (number.precision(true) > (this.params[MAX_NUM_OF_DIGITS_INDEX] ?? MAX_NUM_OF_DIGITS)) {
-      result.addInvalidTypeError("doubleNumber.e002", "The value exceeded maximum number of digits.");
+  validateDigits(number) {
+    const digitThreshold = this.params[MAX_NUM_OF_DIGITS_INDEX] ?? MAX_NUM_OF_DIGITS;
+    if (number.precision(true) > digitThreshold) {
+      this.result.addInvalidTypeError(ERROR_MAP.digitError.code, ERROR_MAP.digitError.message);
     }
-    return result;
   }
 
-  validateDecimals(number, result) {
+  validateDecimals(number) {
     if (number.decimalPlaces() > this.params[MAX_NUM_OF_DECIMALS_INDEX]) {
-      result.addInvalidTypeError("doubleNumber.e003", "The value exceeded maximum number of decimal places.");
+      this.result.addInvalidTypeError(ERROR_MAP.decimalError.code, ERROR_MAP.decimalError.message);
     }
-    return result;
   }
 
   match(value) {
-    let result = new ValidationResult();
-    if (value === null) return result;
+    if (value === null) return this.result;
 
+    // 1 Number conversion
     let number;
     try {
       number = new Decimal(value);
     } catch (e) {
-      result.addInvalidTypeError("doubleNumber.e001", "The value is not a valid decimal number.");
-      return result;
+      this.result.addInvalidTypeError(ERROR_MAP.conversionError.code, ERROR_MAP.conversionError.message);
+      return this.result;
     }
 
-    result = this.validateDigits(number, result);
+    // 2 Digit validation
+    this.validateDigits(number);
 
+    // 3 Decimal validation
     if (this.params.length > DECIMAL_VALIDATION_THRESHOLD) {
-      result = this.validateDecimals(number, result);
+      this.validateDecimals(number);
     }
-    return result;
+
+    return this.result;
 
   }
 }
